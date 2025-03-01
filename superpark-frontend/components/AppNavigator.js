afd516779ffcff52723b84262d91cbdc22c42d69
Shreyas Jenner <shreyasjenner4@gmail.com>
2025-03-01 08:49:04 +0530

    closes #31

--- superpark-frontend/components/AppNavigator.js
+++ superpark-frontend/components/AppNavigator.js
@@ -27,6 +27,7 @@ const validSession = () => {
     if(Platform.OS === 'web') {
         return (localStorage.getItem('token') != null)
     } else {
+        //TEMP CODE TO ALLOW AUTOMATIC LOGIN FOR PHONE
         return true;
     }
 }
